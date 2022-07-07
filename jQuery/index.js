3f972bf7e7b9c7be57199755774e04733353a8ab
Darkgray74 <80634382+Darkgray74@users.noreply.github.com>
2022-07-07 01:57:14 -0300

    create jQuery

---
+++ jQuery/index.js
@@ -0,0 +1,11 @@
+// $("button").click(function() {
+//     $("h1").css("color", "red");
+// });
+
+// $(document).keydown(function(event) {
+//     $("h1").text(event.key);
+// });
+
+$("h1").on("click", function() {
+    $("h1").css("color", "red");
+});
